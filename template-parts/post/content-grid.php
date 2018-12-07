d63f7807991d8024b3afa28803eabbeea1b5337b
Evan Mullins <evan.mullins@get10up.com>
2018-12-07 21:15:26 +1000

    rearrange blog grid content

--- template-parts/post/content-grid.php
+++ template-parts/post/content-grid.php
@@ -20,17 +20,19 @@
 			echo twentyseventeen_time_link();
 			echo '</div><!-- .entry-meta -->';
 		};
-
-		the_title( '<h3 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h3>' );
 		?>
 	</header><!-- .entry-header -->
 
 	<?php if ( '' !== get_the_post_thumbnail() ) : ?>
 		<div class="post-thumbnail">
 			<a href="<?php the_permalink(); ?>">
+				<?php //<img src="http://fpoimg.com/200x200" > ?>
 				<?php the_post_thumbnail( 'twentyseventeen-featured-image' ); ?>
 			</a>
 		</div><!-- .post-thumbnail -->
 	<?php endif; ?>
 
+	<footer class="entry-footer">
+		<?php the_title( '<h3 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h3>' ); ?>
+	</footer>
 </article><!-- #post-## -->
