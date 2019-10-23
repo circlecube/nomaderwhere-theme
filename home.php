f98116688d8783ab2c13fe52926ef574a6150bc5
Evan Mullins <evan.mullins@get10up.com>
2019-10-23 17:21:00 +0100

    blog page template updates

--- home.php
+++ home.php
@@ -3,7 +3,7 @@
  * Template Name: Blog
  *
  * If the user has selected a static page for their blog posts, this is what will
- * appear. 
+ * appear.
  * Learn more: https://codex.wordpress.org/Template_Hierarchy
  *
  * @package WordPress
@@ -16,7 +16,7 @@ get_header(); ?>
 
 <div class="wrap">
 	<div id="primary" class="content-area">
-		<main id="main" class="site-main home" role="main">
+		<main id="main" class="site-main blog" role="main">
 
 			<?php
 			// while ( have_posts() ) : the_post();
@@ -28,33 +28,61 @@ get_header(); ?>
 
 			<div class="latest-grid">
 			<?php
-				//loop and get latest posts - display as a grid of thumbnials with hover state	
 				// WP_Query arguments
+				$paged = ( get_query_var( 'paged' ) ) ? get_query_var( 'paged' ) : 1;
 				$args = array(
-					'posts_per_page' => '99',
+					'posts_per_page' => 99,
 					'post-type'	     => 'post',
+					'pages'          => $paged
 				);
 
 				// The Query
+
 				$query = new WP_Query( $args );
-				
+
 				// The Loop
 				if ( $query->have_posts() ) {
 					while ( $query->have_posts() ) {
 						$query->the_post();
 						// do something
 						$current_month = get_the_date('F');
-						if( $query->current_post === 0 ) { 
-							the_date( 'F Y', '<h2>', '</h2>'  );
+						if( $query->current_post === 0 ) {
+							echo '</div>'; //close grid
+							the_date( 'F Y', '<h2 class="latest-grid-date">', '</h2>'  );
+							echo '<div class="latest-grid">'; //open grid
 						} else {
-							$f = $query->current_post - 1;       
-							$old_date = mysql2date( 'F', $query->posts[$f]->post_date ); 
+							$f = $query->current_post - 1;
+							$old_date = mysql2date( 'F', $query->posts[$f]->post_date );
 							if($current_month != $old_date) {
-								the_date( 'F Y', '<h2>', '</h2>'  );
+								echo '</div>'; //close grid
+								the_date( 'F Y', '<h2 class="latest-grid-date">', '</h2>'  );
+								echo '<div class="latest-grid">'; //open grid
 							}
 						}
 						get_template_part( 'template-parts/post/content', 'grid' );
 					}
+					?>
+					<div class="pagination">
+						<?php
+							echo paginate_links( array(
+								'base'         => str_replace( 999999999, '%#%', esc_url( get_pagenum_link( 999999999 ) ) ),
+								'total'        => $query->max_num_pages,
+								'current'      => max( 1, get_query_var( 'paged' ) ),
+								'format'       => '?paged=%#%',
+								'show_all'     => false,
+								'type'         => 'plain',
+								'end_size'     => 2,
+								'mid_size'     => 1,
+								'prev_next'    => true,
+								'prev_text'    => twentyseventeen_get_svg( array( 'icon' => 'arrow-left' ) ) . '<span class="screen-reader-text">' . __( 'Previous page', 'twentyseventeen' ) . '</span>',
+								'next_text'    => '<span class="screen-reader-text">' . __( 'Next page', 'twentyseventeen' ) . '</span>' . twentyseventeen_get_svg( array( 'icon' => 'arrow-right' ) ),
+								'add_args'     => false,
+								'add_fragment' => '',
+							) );
+						?>
+					</div>
+
+					<?php
 				} else {
 					// no posts found
 				}
